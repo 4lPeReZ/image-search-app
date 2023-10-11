d3e626241baa8518a72b1dea4f2447cd65631f73
4lPeReZ <alvaropr05@hotmail.com>
2023-10-11 12:52:39 +0200

    Html and JS

---
+++ script.js
@@ -0,0 +1,61 @@
+const accessKey = "D-fSGjk0RQDona0stQ4a0M2GZXJqx7EWvb1nFvq7jpk";
+
+const form = document.querySelector("form");
+const input = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.querySelector("#show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages(query) {
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${accessKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.results;
+
+    if (page === 1) {
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+
+    page++;
+
+    if (page > 1) {
+        showMore.style.display = "block";
+    }
+}
+
+function loadRandomImages() {
+    const randomQueries = ["beach", "mountains", "city", "nature"]; // Ejemplos de consultas aleatorias
+    const randomQuery = randomQueries[Math.floor(Math.random() * randomQueries.length)];
+    searchImages(randomQuery);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages(input.value);
+});
+
+showMore.addEventListener("click", () => {
+    searchImages(input.value);
+});
+
+// Cargar imágenes aleatorias al principio
+window.addEventListener("load", loadRandomImages);
